4a725f2f25de880b8ba1d8a4dd00461d672a95a0
Eugene Eronin <everonin@list.ru>
2019-02-22 10:19:48 +0300

    ver5

--- src/App.js
+++ src/App.js
@@ -5,47 +5,55 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    const tre = true;
     this.state = {
-      data: [],
-      tre: 1 };
-      
+       data: [],
+      tre: 'r',
+       flag: true};
+   
 }
    
    
    delta = () =>  {
     fetch('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
     .then(res => res.json())
-    .then(data => this.setState({data: data}));
+    .then(data => this.setState({
+      data: data,
+      tre: 'r'}));
    }
+
    fetchDetails = (e) => {
-    this.setState({tre: e.target.getAttribute('id')});
+     this.setState({tre: e.target , flag: false});
+     
+
+    
     // console.log('We need to get the details for ', data);
   
 }
   
   render() {
-    return (
-      <div>
-      <button onClick={this.delta.bind(this)}>+</button>
-      {/* <div>{JSON.stringify(this.state.data)}</div>  */}
-      <table>
-      <tbody>{this.state.data.map(function(item, key)  {
-             
-            return (
-                                      /*Здесь не работает обработчик onClick={() => this.fetchDetails.bind(item.id)}*/
-                     <tr key = {key}>    
-                      <td>{item.id}</td>
+    const flag = this.state.flag;
+       return((flag)?
+        
+     <div>
+       <button onClick = {this.delta.bind(this)}>+</button>
+        <table>
+      <tbody>{this.state.data.map((item, i) => {
+              
+              return (                         /*Здесь не работает обработчик onClick={() => this.fetchDetails}*/
+                     <tr key = {i} >    
+                      <td onClick={(e) => this.fetchDetails(e)}>{item.id}</td>
                       <td>{item.firstName}</td>
                       <td>{item.lastName}</td>
                       <td>{item.email}</td>
                       <td>{item.phone}</td>
-                  </tr>
-                 )
-             
-             })}</tbody>
+                  </tr>)
+                  
+            })}</tbody>
        </table>
-      </div>
-      );
+
+      </div>: <div>Здесь будут данные строки</div> )
+      
     } 
       
   }
